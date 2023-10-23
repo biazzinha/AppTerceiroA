778a33e034c5b533aee5b289dd31aae3f5632f16
biazinha <beatrizbejarcarvalheiro@gmail.com>
2023-10-23 08:14:47 -0300

    codigo atualizado

--- AppTerceiroA/AppTerceiroA/ComandoSQL.cs
+++ AppTerceiroA/AppTerceiroA/ComandoSQL.cs
@@ -6,6 +6,6 @@ namespace AppTerceiroA
 {
     public static class ComandoSQL
     {
-        public static string insereNome = "INSERT INTO tbl_cliente(id_cliente, nome, sexo) VALUES ('1','Lusca','M')";
+        public static string insereNome = "INSERT INTO tbl_cliente(empresa, telefone, cnpj) VALUES ('FIAP','11-984768502','22.222.222/2211-24');";
     }
 }
